0779683368e80b717b764f02857b104fce77ed28
mattfreg <matthewfregnan97@gmail.com>
2024-09-20 20:34:06 +1000

    Updated website beta

---
+++ Test_Website_Redo_Working_Functionality/knpv.js
@@ -0,0 +1,29 @@
+const songs = [
+    {
+        value: "24K Magic by Bruno Mars",
+        data: {
+            gpath: "1(+1)/8th RB, 1/1st RY, 1/1st B, 3/1st RB, 2/1st G, 1/9th RB",
+            gscore: "132496",
+            g_image: "'24k_magic_by_bruno_mars_guitar.png'",
+            shortname : "24kmagic"
+        }
+    },
+    {
+        value: "abcdefu by GAYLE",
+        data: {
+            gpath: "3/2nd G, 1/NN, 1/NN, 1/13th R, 1/NN, 1/NN, 1/NN",
+            gscore: "137088",
+            g_image: "'abcdefu_by_gayle_guitar.png'",
+            shortname : "abcdefu"
+        }
+    },
+    {
+        value: "Ain't No Rest For The Wicked by Cage The Elephant",
+        data: {
+            gpath: "1/1st YB, 1/3rd YB, 1/5th GR, 1/1st YB, 1/1st GY, 1/1st GR, 1/4th B, 1/3rd YB, 1/1st GY, 1/1st GR",
+            gscore: "190386",
+            g_image: "'aint_no_rest_for_the_wicked_by_cage_the_elephant_guitar.png'",
+            shortname : "aintnorestforthewicked"
+        }
+    }
+];
